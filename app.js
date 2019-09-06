62711d375f8a79da5e0d681b2a8f83480bfba045
Aydee-QM <aydee.qm@gmail.com>
2019-09-06 11:50:16 -0500

    competed conexion to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost:27017/fotos",  {useNewUrlParser: true })
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.log(err));;
+
+const userSchemaJSON = {
+  email: String,
+  password: String
+};
+
+const user_schema = new Schema(userSchemaJSON);
+
+const User = mongoose.model("User", user_schema);
+
+app.use('/public',express.static('public'));
+app.use(bodyParser.json()); // para peticiones aplicacion/json
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine', 'pug')
 
@@ -9,7 +28,20 @@ app.get('/', (request, response) => {
 });
 
 app.get('/login', (request, response) => {
-  response.render('login')
+  User.find((err, doc) => {
+    console.log(doc);
+    response.render('login');
+  });
+});
+
+app.post('/users', (request, response) => {
+  const user = new User({
+    email: request.body.email,
+    password: request.body.password
+  });
+  user.save(() => {
+    response.send('Recibimos tus datos');
+  });
 });
 
 app.listen('8080', () => {
